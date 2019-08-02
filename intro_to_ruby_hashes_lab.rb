bac2c537a15b0af8114084e9e66da5376c33ba3f
Candice Peters <candicelizabethpeters@gmail.com>
2019-08-02 17:23:48 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -13,8 +13,8 @@ monopoly = {:railroads =>
               {:pieces => 4,
               :rent_in_dollars => {
                 :one_piece_owned => 25,
-                :hash_two => "",
-                :hash_three => "",
+                :two_pieces_owned => 50,
+                :three_pieces_owned => 100,
                 :hash_four => ""
               },
               :names => {
